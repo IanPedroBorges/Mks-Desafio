ee9c54cbe16813515339e6c8d7cab56c8edeea6b
IanPedroBorges <ianhudback@gmail.com>
2024-04-26 21:54:23 -0300

    create service

---
+++ src/services/GetProducts.ts
@@ -0,0 +1,7 @@
+import { ProductType } from "../types/productType";
+
+export const getProducts = async () => {
+    const response = await fetch("https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=10&sortBy=id&orderBy=DESC");
+    const data = await response.json();
+    return data.products as ProductType[];
+};
